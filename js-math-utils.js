f667f50f4eb4c80e032aedeadd483df30c9e309d
SantiagoBaezDuic <santib1598@hotmail.com>
2021-11-25 21:34:55 -0300

    Agregamos el resto de las funciones basicas

---
+++ js-math-utils.js
@@ -0,0 +1,17 @@
+const sum = (num1, num2) => {
+    return console.log(num1 + num2);
+}
+
+const substract = (num1, num2) => {
+    return console.log(num1 - num2);
+}
+
+const multiply = (num1, num2) => {
+    return console.log(num1 * num2);
+}
+
+const divide = (num1, num2) => {
+    return console.log(num1 / num2);
+}
+
+divide(9, 3);
